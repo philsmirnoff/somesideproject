40fbbab44cc0f93549ed8ba375deb5547b5be9f1
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-02-10 23:34:32 -0500

    fixing the bug with addTodo

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -10,13 +10,14 @@ function TodoForm(props) {
   const handleSubmit = e => {
     e.preventDefault();
 
-    // props.onSubmit({
-    //   id: Math.floor(Math.random() * 10000),
-    //   text: input
-    // })
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
 
     setInput('');
-  }
+  };
+
   return (
       <form className="todo-form" onSubmit={handleSubmit}>
         <input
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,10 +5,19 @@ import TodoForm from './TodoForm';
 function TodoList() {
   const [todos, setTodos] = useState([]);
 
+  const addTodo = todo => {
+    if(!todo.text || /^\s*$/.test(todo.text)) {
+      return
+    }
+
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+    console.log(...todos);
+  }
   return (
   <div>
     <h1>Whats your plan for today?</h1>
-    <TodoForm />
+    <TodoForm onSubmit={addTodo}/>
   </div>
   )
 }
